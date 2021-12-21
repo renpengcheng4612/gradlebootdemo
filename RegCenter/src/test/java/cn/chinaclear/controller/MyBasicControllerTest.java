1ae33039e46b5d6e4b889f01b228f08d745baeaa
焱宣 <871340705@qq.com>
2021-12-21 16:53:47 +0800

    调通单元测试

--- RegCenter/src/test/java/cn/chinaclear/controller/MyBasicControllerTest.java
+++ RegCenter/src/test/java/cn/chinaclear/controller/MyBasicControllerTest.java
@@ -3,8 +3,10 @@ package cn.chinaclear.controller;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class MyBasicControllerTest {
 
     @Autowired
@@ -20,16 +23,14 @@ public class MyBasicControllerTest {
 
 
     @Before
-    void setUp() {
+    public void setUp() {
     mockMvc= MockMvcBuilders.webAppContextSetup(context).build();
     }
 
     @Test
-    void basicMethod() throws Exception {
-
+    public void basicMethod() throws Exception {
         ResultActions perform = mockMvc.perform(MockMvcRequestBuilders.get("/test1"));
-
-
+        System.out.println(perform.toString());
     }
 
 }
